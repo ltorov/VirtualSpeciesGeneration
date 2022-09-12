2ee816534338bc116693ea516150aca40a4a73b2
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2022-09-12 18:29:02 -0500

    Add files via upload

--- BetaDeformations.m
+++ BetaDeformations.m
@@ -1,37 +1,31 @@
-NumLayers = 19;
-n = 1000;
-x1 = linspace(0,1,n)'; x2 = linspace(0,1,n)';
-X = zeros(NumLayers, n);
+function WarpedSpace = BetaDeformations(X,NumLayers,n)
 
-for i = 1:NumLayers
-    X(i,:) = linspace(0,1,n);
-end
-
-G = zeros(NumLayers, n);
-
-a = 5.*rand(NumLayers,1);
-b = 5.*rand(NumLayers,1);
-
-for i = 1:NumLayers
-    if a(i,:)<1
-        if b(i,:)>=1
-            b(i,:) = rand(1);
+    G = zeros(NumLayers, n);
+    
+    a = 5.*rand(NumLayers,1);
+    b = 5.*rand(NumLayers,1);
+    
+    for i = 1:NumLayers
+        if a(i,:)<1
+            if b(i,:)>=1
+                b(i,:) = rand(1);
+            end
+        end
+        if a(i,:)>=1
+            if b(i,:)<1
+                b(i,:) = 1 +6.*rand(1);
+            end
         end
     end
-    if a(i,:)>=1
-        if b(i,:)<1
-            b(i,:) = 1 +6.*rand(1);
-        end
+    
+    for i = 1:NumLayers
+        G(i,:) = betacdf(X(i,:),a(i),b(i));
     end
-end
-
-for i = 1:NumLayers
-    G(i,:) = betacdf(X(i,:),a(i),b(i));
-end
-
-%%
-hold on
-for i =1:NumLayers
-    g = G(i,:);
-    plot(X(i,:),g)
+    
+    %%
+    hold on
+    for i =1:NumLayers
+        g = G(i,:);
+        plot(X(i,:),g)
+    end
 end
